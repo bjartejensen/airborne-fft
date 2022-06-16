f8f504685b4c183e63b699d79fe6cce505d3c881
Bjarte Michel Jensen <bjartejensen@gmail.com>
2022-06-16 12:21:48 +0200

    renaming and tree-shaking

--- apps/airborne/src/app/app.module.ts
+++ apps/airborne/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { FrontpageModule } from './frontpage/frontpage.module';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
